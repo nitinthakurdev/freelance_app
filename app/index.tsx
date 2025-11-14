a2ef25e94166b3643eaeee7e648efd4392aec659
nitinthakurdev <radheradhe9870023@gmail.com>
2025-11-14 22:50:00 +0530

    Setup project

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View className='h-full w-full items-center justify-center' >
+      <Text className='text-3xl' >index</Text>
+    </View>
+  )
+}
+
+export default index
